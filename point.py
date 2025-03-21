5e467e28c22f1c66ffab811b3f75d291f0176f4c
matilderibeiro <matilde.csribeiro@gmail.com>
2025-03-21 12:23:21 +0100

    Added more stuff

--- point.py
+++ point.py
@@ -38,60 +38,61 @@ class Point:
     def __eq__(self, other):
         return self.distance_orig() == other.distance_orig()
 
-points = []
-for i in range(5):
-    p = Point(
-        random.randint(-100, 100),
-        random.randint(-100, 100)
-    )
+if __name__=="__main__":
+    points = []
+    for i in range(5):
+        p = Point(
+            random.randint(-100, 100),
+            random.randint(-100, 100)
+        )
 
-    # Append it to the list
-    points.append(p)
+        # Append it to the list
+        points.append(p)
 
-for point in points:
+    for point in points:
+        print(points)
+
+    # points.sort()
+    p = Point(-12,-5)
+    print(p.distance_orig())
+
+    p1 = Point(1, 2) # Create a new instance
+    p2 = Point(3,4)
+    p3 = Point("James", "Jane") # This is valid but probably not intended
+
+    print(p1.x, p1.y) # Access attributes
+    print(p1)
+
+    p1 = (4,6)
+    p2 = (7,7)
+
+    print(p1.__gt__(p2))
+
+    print("unsorted points")
+    print(points)
+    print("sorted points")
+    points.sort()
     print(points)
 
-# points.sort()
-p = Point(-12,-5)
-print(p.distance_orig())
+    found_equal = 0
+    count = 0
+    while True:
+        if found_equal == 10:
+            break
+        p1 = Point(
+            random.randint(-100, 100),
+            random.randint(-100, 100)
+        )
 
-p1 = Point(1, 2) # Create a new instance
-p2 = Point(3,4)
-p3 = Point("James", "Jane") # This is valid but probably not intended
+        p2 = Point(
+            random.randint(-100, 100),
+            random.randint(-100, 100)
+        )
 
-print(p1.x, p1.y) # Access attributes
-print(p1)
+        count += 1
+        if p1 == p2:
+            print(p1, p2)
+            found_equal +=1
 
-p1 = (4,6)
-p2 = (7,7)
-
-print(p1.__gt__(p2))
-
-print("unsorted points")
-print(points)
-print("sorted points")
-points.sort()
-print(points)
-
-found_equal = 0
-count = 0
-while True:
-    if found_equal == 10:
-        break
-    p1 = Point(
-        random.randint(-100, 100),
-        random.randint(-100, 100)
-    )
-
-    p2 = Point(
-        random.randint(-100, 100),
-        random.randint(-100, 100)
-    )
-
-    count += 1
-    if p1 == p2:
-        print(p1, p2)
-        found_equal +=1
-
-print(f"probability is 1 in {count/found_equal}")
+    print(f"probability is 1 in {count/found_equal}")
 
